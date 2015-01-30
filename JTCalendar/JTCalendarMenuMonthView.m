9e1a8746fd80511819206ac39b8cd1eb15b163e6
Yuri Ferretti <yuriferretti@gmail.com>
2015-01-30 10:58:26 -0200

    refactoring

--- JTCalendar/JTCalendarMenuMonthView.m
+++ JTCalendar/JTCalendarMenuMonthView.m
@@ -12,10 +12,6 @@
     
 }
 
-@property (assign, nonatomic) BOOL autoDissolve;
-@property (assign, nonatomic) CGFloat autoDissolveMinAlpha;
-@property (assign, nonatomic) CGFloat autoDissolveMaxAlpha;
-
 @end
 
 @implementation JTCalendarMenuMonthView
@@ -60,18 +56,6 @@
     textLabel.text = self.calendarManager.calendarAppearance.monthBlock(currentDate, self.calendarManager);
 }
 
-- (void)setAutoDissolve:(BOOL)autoDissolve
-{
-    if (_autoDissolve != autoDissolve) {
-        _autoDissolve = autoDissolve;
-        if (_autoDissolve) {
-            [self.calendarManager.menuMonthsView addObserver:self forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew context:nil];
-        } else {
-            [self.calendarManager.menuMonthsView removeObserver:self forKeyPath:@"contentOffset"];
-        }
-    }
-}
-
 - (void)layoutSubviews
 {
     textLabel.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height);
@@ -83,24 +67,7 @@
 {
     textLabel.textColor = self.calendarManager.calendarAppearance.menuMonthTextColor;
     textLabel.font = self.calendarManager.calendarAppearance.menuMonthTextFont;
-    _autoDissolveMinAlpha = self.calendarManager.calendarAppearance.autoDissolveMinAlpha;
-    _autoDissolveMaxAlpha = self.calendarManager.calendarAppearance.autoDissolveMaxAlpha;
-    self.autoDissolve = self.calendarManager.calendarAppearance.autoDissolveMenu;
 }
 
-- (void)dealloc
-{
-    [self.calendarManager.menuMonthsView removeObserver:self forKeyPath:@"contentOffset"];
-}		 
-
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
-{
-        if ([keyPath isEqualToString:@"contentOffset"]) {
-                CGFloat offsetX = [self convertPoint:CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2) toView:self.window].x;
-                CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
-                CGFloat alpha = _autoDissolveMaxAlpha - (ABS(screenWidth/2-offsetX)/(screenWidth/2))*(_autoDissolveMaxAlpha-_autoDissolveMinAlpha);
-                self.alpha = alpha;
-        }
-}
 
 @end
